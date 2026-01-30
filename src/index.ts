b9566bccba6e76c603975ffb9a72e2b1c714144f
arunkumar-source <arunkumar@webzenith.tech>
2026-01-30 21:53:15 +0530

    main3

--- src/index.ts
+++ src/index.ts
@@ -58,4 +58,8 @@ app.delete("/delete/:id", (c) => {
   return c.body(null, 204)
 })
 
-export default handle(app)
+export const GET = handle(app)
+export const POST = handle(app)
+export const PATCH = handle(app)
+export const DELETE = handle(app)
+export const OPTIONS = handle(app)
