c681a13877d895427166c84f42a6634283151a95
arunkumar-source <arunkumar@webzenith.tech>
2026-01-30 22:17:00 +0530

    main

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ app.options('*', (c) => {
 let works: Work[] = []
 
 app.use('*', cors({
-  origin: ['http://localhost:5173','https://vercel.com/arun-kumars-projects-67b3a0ee/work-project/AYbXk9SjQPGGUWAZHLiY2raeDs5D'], 
+  origin: ['http://localhost:5173','https://work-project-k4hjud9l2-arun-kumars-projects-67b3a0ee.vercel.app/'], 
   allowMethods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowHeaders: ['Content-Type'],
 }))
